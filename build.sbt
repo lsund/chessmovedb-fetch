607c8decd36b45362c4ed4970d0278b4f8c101a4
Ludvig Sundström <ludvig.sundstroem@innoq.com>
2019-10-11 12:59:41 +0200

    Use local pgnparser

--- build.sbt
+++ build.sbt
@@ -2,5 +2,10 @@ scalaVersion := "2.13.1"
 name := "hello-world"
 organization := "ch.epfl.scala"
 version := "1.0"
+
+// Dependencies
 libraryDependencies += "org.typelevel" %% "cats-core" % "2.0.0"
 libraryDependencies +=  "org.scalaj" %% "scalaj-http" % "2.4.2"
+libraryDependencies += "com.github.pathikrit" %% "better-files" % "3.8.0"
+
+libraryDependencies += "com.github.lsund" % "pgnparser" % "1.0.0" from "file:///home/lsund/Documents/git/pgnparser/target/scala-2.13/pgnparser_2.13-1.0.0.jar"
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,8 +1,18 @@
 import scalaj.http._
+import scala.sys.process._
+import java.nio.file.{Paths, Files}
+import java.nio.charset.StandardCharsets
+import com.github.lsund.pgnparser._
 
 object Main extends App {
-  val request: HttpRequest = Http("http://example.com")
-  val response: HttpResponse[String] =
-    request.asString
-  println(response);
+  val gameid = "q6NY3GkB"
+  val apiToken: String = "pass lichess/api-token".!!.trim
+  val authorizationHeaderValue: String = "Bearer " + apiToken
+  val request: HttpRequest = Http("https://lichess.org/game/export/" + gameid)
+    .header("Authorization", authorizationHeaderValue)
+  val response: HttpResponse[String] = request.asString
+  Files.write(
+    Paths.get("data/" + gameid + ".pgn"),
+    response.body.getBytes(StandardCharsets.UTF_8)
+  )
 }
