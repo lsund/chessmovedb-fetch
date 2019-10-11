7bff71960665ff889ee9f8f911171594046795df
Ludvig Sundström <ludvig.sundstroem@innoq.com>
2019-10-11 13:29:22 +0200

    Create data directory

--- build.sbt
+++ build.sbt
@@ -1,7 +1,7 @@
 scalaVersion := "2.13.1"
-name := "hello-world"
-organization := "ch.epfl.scala"
-version := "1.0"
+name := "gamefetcher"
+organization := "com.github.lsund"
+version := "1.0.0"
 
 // Dependencies
 libraryDependencies += "org.typelevel" %% "cats-core" % "2.0.0"
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,8 +1,10 @@
+package com.github.lsund.gamefetcher
+
 import scalaj.http._
 import scala.sys.process._
-import java.nio.file.{Paths, Files}
-import java.nio.charset.StandardCharsets
+import java.nio.file.{Paths, Files}, java.nio.charset.StandardCharsets
 import com.github.lsund.pgnparser._
+import better.files._, better.files.File._
 
 object Main extends App {
   val gameid = "q6NY3GkB"
@@ -11,6 +13,7 @@ object Main extends App {
   val request: HttpRequest = Http("https://lichess.org/game/export/" + gameid)
     .header("Authorization", authorizationHeaderValue)
   val response: HttpResponse[String] = request.asString
+  val dataDir = "data".toFile.createIfNotExists(true)
   Files.write(
     Paths.get("data/" + gameid + ".pgn"),
     response.body.getBytes(StandardCharsets.UTF_8)
